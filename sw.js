50a64044bb9fc83ac87a92aa64136cf28e0cc82d
demellogabriel2015-max <demellogabriel2015@gmail.com>
2026-07-20 20:47:39 -0300

    Fix sw.js v10

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE_NAME = 'invest-v10-engine';
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => e.waitUntil(caches.keys().then(ks => Promise.all(ks.map(k => caches.delete(k)))).then(() => self.clients.claim())));
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request)));
