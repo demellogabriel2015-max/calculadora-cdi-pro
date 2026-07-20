4c91e1420469f8b205882c1dcf7294a47773b85a
demellogabriel2015-max <demellogabriel2015@gmail.com>
2026-07-20 20:34:02 -0300

    Force cache update for CDI 14.15% (v5.8.2)

--- sw.js
+++ sw.js
@@ -1,4 +1,14 @@
-const CACHE_NAME = 'invest-master-v5.7.5';
-const assets = ['./', './index.html', './manifest.json'];
-self.addEventListener('install', (e) => e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(assets))));
-self.addEventListener('fetch', (e) => e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request))));
+const CACHE_NAME = 'invest-master-v5.8.2-CDI-FIX';
+const assets = ['./index.html', './manifest.json'];
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(assets)));
+});
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then((keys) => Promise.all(
+    keys.map((k) => k !== CACHE_NAME && caches.delete(k))
+  )));
+});
+self.addEventListener('fetch', (e) => e.respondWith(
+  fetch(e.request).catch(() => caches.match(e.request))
+));
