d20ec140c76785b8a57017bec5c19f2a9b52d170
demellogabriel2015-max <demellogabriel2015@gmail.com>
2026-07-20 21:09:01 -0300

    Cache Killer v17

--- sw.js
+++ sw.js
@@ -1,4 +1,13 @@
-const CACHE_NAME = 'invest-v12-bypass';
-self.addEventListener('install', e => self.skipWaiting());
-self.addEventListener('activate', e => e.waitUntil(caches.keys().then(ks => Promise.all(ks.map(k => caches.delete(k)))).then(() => self.clients.claim())));
-self.addEventListener('fetch', e => e.respondWith(fetch(e.request)));
+const CACHE_NAME = 'nuclear-cleanup-v17';
+self.addEventListener('install', e => {
+    self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+    e.waitUntil(
+        caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+        .then(() => self.clients.claim())
+    );
+});
+self.addEventListener('fetch', e => {
+    e.respondWith(fetch(e.request));
+});
